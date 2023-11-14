0cde2011993cb2dbc8df85092889ad2c291afae0
mattsloe <mattsloe@gmail.com>
2023-11-14 01:13:43 -0800

    Created App page with styling, created LandingCard component where user inputs destination.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import LandingCard from './Card';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+  const [showCard, setShowCard] = useState(true);
+  const [destination, setDestination] = useState(''); //state to store destination
+
+
+  const handleGoClick = (inputValue) => {
+    setDestination(inputValue); //update state with input value
+    setShowCard(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Container fluid className="App">
+      {showCard ? <LandingCard onGoClick={handleGoClick} /> : <h1>Destination: {destination}</h1>}
+      {/*displays destination info when card not shown*/}
+    </Container>
+
   );
 }
 
