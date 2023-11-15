8882510f348388522edd9805728da36ee2d54356
mahadghani <mghani@pdx.edu>
2023-11-15 14:47:11 -0800

    Commit

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+// src/components/Search.js
+import React, { useState } from "react";
+
+const Search = () => {
+  return (
+    <div className="container mt-5 parent-info">
+      <h1 className="text-center display-4">Search for a trip</h1>
+    </div>
+  );
+};
+
+export default Search;
